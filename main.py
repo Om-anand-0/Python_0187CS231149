787a5c080713fb4a1757d3521a80122d5c7eeb23
Om-anand-0 <om.d3v.21@gmail.com>
2025-12-01 19:08:04 +0000

    Made clases for fetching user and repos (not tested yet)

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("✅working✅")
+import requests
+
+class GithubAPI:
+    Base_Url = "https://api.github.com/users/"
+    
+    def get_user(self,username):
+        url = f"{self.Base_url}{username}"
+        response = requests.get(url)
+        response.raise_for_status()
+        return respons.json()
+    
+    def get_repos(self,name):
+        url = f"{self.Base_url}{username}/repos?per_page=100"
+        response = requests.get(url)
+        response.raise_for_status()
+        return respons.json()
