729efe051ba411e2909edeae039881d563a4ee1e
Om-anand-0 <om.d3v.21@gmail.com>
2025-12-02 09:01:08 +0000

    renamed main to github api for better code readability

---
+++ github_api.py
@@ -0,0 +1,17 @@
+import requests
+
+class GithubAPI:
+    Base_Url = "https://api.github.com/users/"
+    
+    def get_user(self,username):
+        url = f"{self.Base_Url}{username}"
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    
+    def get_repos(self,username):
+        url = f"{self.Base_Url}{username}/repos?per_page=100"
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    
