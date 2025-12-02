632f634cd7c971bb4623ac0664ee0d5eb89b1e15
Om-anand-0 <om.d3v.21@gmail.com>
2025-12-02 09:52:52 +0000

    to store information of repos

---
+++ repo.py
@@ -0,0 +1,17 @@
+class Repo:
+    def __init__(self, data):
+        self.name = data.get("name")
+        self.language = data.get("language")
+        self.stars = data.get("stargazers_count")
+        self.forks = data.get("forks_count")
+        self.size = data.get("size")
+
+    def summary(self):
+        return {
+            "name": self.name,
+            "language": self.language,
+            "stars": self.stars,
+            "forks": self.forks,
+            "size_kb": self.size
+        }
+
