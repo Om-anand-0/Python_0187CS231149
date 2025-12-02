a1738add48c301f33071326f3b7ae58bd3f890be
Om-anand-0 <om.d3v.21@gmail.com>
2025-12-02 07:34:21 +0000

    working point, can print repos name and no of repos

--- main.py
+++ main.py
@@ -4,13 +4,30 @@ class GithubAPI:
     Base_Url = "https://api.github.com/users/"
     
     def get_user(self,username):
-        url = f"{self.Base_url}{username}"
+        url = f"{self.Base_Url}{username}"
         response = requests.get(url)
         response.raise_for_status()
-        return respons.json()
+        return response.json()
     
-    def get_repos(self,name):
-        url = f"{self.Base_url}{username}/repos?per_page=100"
+    def get_repos(self,username):
+        url = f"{self.Base_Url}{username}/repos?per_page=100"
         response = requests.get(url)
         response.raise_for_status()
-        return respons.json()
+        return response.json()
+    
+
+def main():
+    username = input("Enter Github Username :")
+
+    api = GithubAPI()
+    user_data = api.get_user(username)
+    #user = GithubUser(user_data)
+    
+    repo_data = api.get_repos(username)
+    repo_count = len(repo_data)
+    repo_names = [r["name"] for r in repo_data]
+    print(repo_names) 
+    print("number of repos are : ", repo_count)
+
+if __name__ == "__main__":
+    main()
