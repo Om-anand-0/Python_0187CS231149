f9ca142f22bf0fc3aad228e0841b54c944326540
Om-anand-0 <om.d3v.21@gmail.com>
2025-12-07 20:59:23 +0000

    Added headers in api calls and requirements file

--- github_api.py
+++ github_api.py
@@ -2,18 +2,20 @@
 
 import requests
 
+
 class GithubAPI:
     Base_Url = "https://api.github.com/users/"
+    headers = {"User-Agent": "Github-User-Analyzer"}
     
     def get_user(self,username):
         url = f"{self.Base_Url}{username}"
-        response = requests.get(url)
+        response = requests.get(url, headers=self.headers)
         response.raise_for_status()
         return response.json()
     
     def get_repos(self,username):
         url = f"{self.Base_Url}{username}/repos?per_page=100"
-        response = requests.get(url)
+        response = requests.get(url,headers=self.headers)
         response.raise_for_status()
         return response.json()
     
